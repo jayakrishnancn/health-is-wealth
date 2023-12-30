d904cfc280173b6478701a25172d6731ef79316d
jayakrishnancn <jayakrishnancn@gmail.com>
2023-12-30 22:44:10 +0530

    color code hip ratio

--- src/app/utils/calculations/waist-hip-ratio.ts
+++ src/app/utils/calculations/waist-hip-ratio.ts
@@ -1,4 +1,4 @@
-import { CalculateResultType, CalculationMethodResult, Measurements } from "./type";
+import { CalculateResultType, CalculationMethodResult, HealthRiskColorCode, Measurements } from "./type";
 
 const isValidInputs = (inputs: Measurements) => inputs.waistInCM && inputs.sex
 
@@ -9,15 +9,17 @@ export const waist_hip_ratio = (inputs: Measurements): CalculateResultType | nul
     }
 
     const idealRatio = inputs.sex === "Female" ? 0.85 : 0.90
-    const result = idealRatio * inputs.waistInCM
+    const idealHipInCm = idealRatio * inputs.waistInCM
 
+    const colorCode = inputs.heightInCM > 0 ? HealthRiskColorCode.UNKNOWN : inputs.hipInCM < idealHipInCm ? HealthRiskColorCode.Low : inputs.hipInCM > idealHipInCm ? HealthRiskColorCode.High : HealthRiskColorCode.Average
     const methods = [{
         name: "waist to hip ratio",
         label: "waist to hip ratio",
-        result,
+        result: idealHipInCm,
         Unit: "cm",
         status: "Ideal ratio for Female = 0.85 , Male = 0.90",
-        notes_or_details: "Ratio = W / H. https://www.omnicalculator.com/health/waist-hip-ratio"
+        notes_or_details: "Ratio = W / H. https://www.omnicalculator.com/health/waist-hip-ratio",
+        colorCode
     }] as CalculationMethodResult[]
 
 
